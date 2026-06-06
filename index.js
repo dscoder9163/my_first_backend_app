21980613a65e3bb29a872474b2c1e75bacee9410
Debasish <debasish.santra9163@gmail.com>
2026-06-06 11:33:39 +0530

    demodata json added

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const demodata = {
+    "name": "Adeel Solangi",
+    "language": "Sindhi",
+    "id": "V59OF92YF627HFY0",
+    "bio": "Donec lobortis eleifend condimentum. Cras dictum dolor lacinia lectus vehicula rutrum. Maecenas quis nisi nunc. Nam tristique feugiat est vitae mollis. Maecenas quis nisi nunc.",
+    "version": 6.1
+  }
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -15,6 +22,11 @@ app.get('/about', (req, res) => {
 app.get('/login',(req, res) => {
   res.end('<h1>Please login this page</h1>')
 })
+
+app.get('/demo',(req, res) => {
+  res.json(demodata);
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
 })
